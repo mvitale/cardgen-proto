753c7467ee1e2827f6e09259ebc93375437566c8
Michael Vitale <mmvitale@gmail.com>
2017-02-02 14:57:38 -0500

    express server skeleton w json parsing

--- server.js
+++ server.js
@@ -1,13 +1,32 @@
-var express = require("express");
-var app = express();
-var port = 8080;
+const port = 8080;
+const mongoPort = 27017;
+
+var express    = require('express');
+var app        = express();
+var bodyParser = require('body-parser');
+
+var MongoClient = require('mongodb').MongoClient;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+var cardsDb = null;
+MongoClient.connect('mongodb://localhost:' + mongoPort + '/cardtest', function (err, db) {
+  if (err) throw err;
+  cardsDb = db;
+});
 
 // Routes
 var router = express.Router();
 
 // Test route
-router.get('/', function(req, res) {
-  res.json({ message: 'API functioning!' });
+router.get('/ping', function(req, res) {
+  res.json({ message: 'I\'m up!' });
+});
+
+router.post('/save', function(req, res) {
+  console.log(req.body);
+  res.json(req.body);
 });
 
 // Register all routes at /
@@ -15,4 +34,5 @@ app.use('/', router);
 
 // Start server
 app.listen(port);
-console.log('Make requests to port ' + port);
+console.log('Server running on port ' + port);
+

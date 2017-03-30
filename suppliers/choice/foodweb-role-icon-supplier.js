a3356787f19926899887ceefa1982675dde5497a
Michael Vitale <mmvitale@gmail.com>
2017-03-30 16:47:02 -0400

    use pngs for all icons

--- suppliers/choice/foodweb-role-icon-supplier.js
+++ suppliers/choice/foodweb-role-icon-supplier.js
@@ -2,13 +2,13 @@ var urlHelper = require('../../url-helper');
 
 module.exports.supply = function(params, apiResults, cb) {
   cb(null, [
-    { label: 'Apex predator', url: urlHelper.staticImageUrl('foodweb_icons/APEX.svg')},
-    { label: 'Autotroph', url: urlHelper.staticImageUrl('foodweb_icons/AUTO.svg')},
-    { label: 'Carnivore', url: urlHelper.staticImageUrl('foodweb_icons/CARN.svg')},
-    { label: 'Decomposer', url: urlHelper.staticImageUrl('foodweb_icons/DECO.svg')},
-    { label: 'Detritivore', url: urlHelper.staticImageUrl('foodweb_icons/DETR.svg')},
-    { label: 'Herbivore', url: urlHelper.staticImageUrl('foodweb_icons/HERB.svg')},
-    { label: 'Omnivore', url: urlHelper.staticImageUrl('foodweb_icons/OMNI.svg')},
-    { label: 'Sanguinivore', url: urlHelper.staticImageUrl('foodweb_icons/SANG.svg')}
+    { label: 'Apex predator', url: urlHelper.staticImageUrl('foodweb_icons/APEX.png')},
+    { label: 'Autotroph', url: urlHelper.staticImageUrl('foodweb_icons/AUTO.png')},
+    { label: 'Carnivore', url: urlHelper.staticImageUrl('foodweb_icons/CARN.png')},
+    { label: 'Decomposer', url: urlHelper.staticImageUrl('foodweb_icons/DECO.png')},
+    { label: 'Detritivore', url: urlHelper.staticImageUrl('foodweb_icons/DETR.png')},
+    { label: 'Herbivore', url: urlHelper.staticImageUrl('foodweb_icons/HERB.png')},
+    { label: 'Omnivore', url: urlHelper.staticImageUrl('foodweb_icons/OMNI.png')},
+    { label: 'Sanguinivore', url: urlHelper.staticImageUrl('foodweb_icons/SANG.png')}
   ]);
 }
--- suppliers/choice/iucn-icon-supplier.js
+++ suppliers/choice/iucn-icon-supplier.js
@@ -2,12 +2,12 @@ var urlHelper = require('../../url-helper');
 
 module.exports.supply = function(params, apiResults, cb) {
   cb(null, [
-    { label: 'Least concern (LC)', url: urlHelper.staticImageUrl('iucn_icons/LC.svg') },
-    { label: 'Near threatened (NT)', url: urlHelper.staticImageUrl('iucn_icons/NT.svg') },
-    { label: 'Vulnerable (VU)', url: urlHelper.staticImageUrl('iucn_icons/VU.svg') },
-    { label: 'Endangered (EN)', url: urlHelper.staticImageUrl('iucn_icons/EN.svg') },
-    { label: 'Critically endangered (CR)', url: urlHelper.staticImageUrl('iucn_icons/CR.svg') },
-    { label: 'Extinct in the wild (EW)', url: urlHelper.staticImageUrl('iucn_icons/EW.svg') },
-    { label: 'Extinct (EX)', url: urlHelper.staticImageUrl('iucn_icons/EX.svg') }
+    { label: 'Least concern (LC)', url: urlHelper.staticImageUrl('iucn_icons/LC.png') },
+    { label: 'Near threatened (NT)', url: urlHelper.staticImageUrl('iucn_icons/NT.png') },
+    { label: 'Vulnerable (VU)', url: urlHelper.staticImageUrl('iucn_icons/VU.png') },
+    { label: 'Endangered (EN)', url: urlHelper.staticImageUrl('iucn_icons/EN.png') },
+    { label: 'Critically endangered (CR)', url: urlHelper.staticImageUrl('iucn_icons/CR.png') },
+    { label: 'Extinct in the wild (EW)', url: urlHelper.staticImageUrl('iucn_icons/EW.png') },
+    { label: 'Extinct (EX)', url: urlHelper.staticImageUrl('iucn_icons/EX.png') }
   ]);
 };

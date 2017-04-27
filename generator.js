9cf611189d73a71862ed885a8e18dcb36005f075
Michael Vitale <mmvitale@gmail.com>
2017-04-27 10:31:24 -0400

    update generator to use new TemplateRenderer constructor

--- generator.js
+++ generator.js
@@ -3,13 +3,10 @@
  */
 
 var templateReader = require('./template-reader');
-var templateRenderer = require('./template-renderer/template-renderer');
+var TemplateRenderer = require('./template-renderer/template-renderer');
 var svgCanvasSupplier = require('./svg-canvas-supplier');
 var pngCanvasSupplierFactory = require('./png-canvas-supplier-factory');
 var imageFetcher = require('./image-fetcher');
-
-templateRenderer.setTemplateSupplier(templateReader);
-templateRenderer.setImageFetcher(imageFetcher);
 
 /*
  * Takes a Card and generates an SVG
@@ -22,8 +19,10 @@ templateRenderer.setImageFetcher(imageFetcher);
  *   A Buffer containing the SVG data
  */
 module.exports.generateSvg = function(card, cb) {
-  templateRenderer.setCanvasSupplier(svgCanvasSupplier);
-  generateHelper(card, cb);
+  var renderer =
+    new TemplateRenderer(templateReader, svgCanvasSupplier, imageFetcher);
+
+  generateHelper(card, renderer, cb);
 }
 
 /*
@@ -35,20 +34,21 @@ module.exports.generateSvg = function(card, cb) {
  *
  * Result:
  *   A Buffer containing the PNG data
- */
+ *
 module.exports.generatePng = function(card, width, cb) {
   var canvasSupplier = pngCanvasSupplierFactory.instance(width);
   templateRenderer.setCanvasSupplier(canvasSupplier);
   generateHelper(card, cb);
 }
+*/
 
-function generateHelper(card, cb) {
-  templateRenderer.setCard(card, (err) => {
+function generateHelper(card, renderer, cb) {
+  renderer.setCard(card, (err) => {
     if (err) {
       return cb(err);
     }
 
-    templateRenderer.draw((err, canvas) => {
+    renderer.draw((err, canvas) => {
       if (err) return cb(err);
 
       cb(null, canvas.toBuffer());

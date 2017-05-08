8d53da30e7cddac333635d86c0652efcf2851f96
Michael Vitale <mmvitale@gmail.com>
2017-05-08 09:28:26 -0400

    default supplier util test

--- suppliers/default/util/util.js
+++ suppliers/default/util/util.js
@@ -1,11 +1,18 @@
 module.exports.extractClassName = function(apiResults) {
   var ancestors = apiResults.hierarchy_entries.ancestors
-    , classLevelResults = ancestors.filter((result) => {
-        return result.taxonRank === 'class'
-      })
-    , result = classLevelResults.length ?
-        classLevelResults[0].scientificName : null;
+    , classLevelResults
+    , result = ''
     ;
+
+  if (ancestors) {
+    classLevelResults = ancestors.filter((result) => {
+      return result.taxonRank === 'class'
+    });
+
+    if (classLevelResults.length && classLevelResults[0].scientificName) {
+      result = classLevelResults[0].scientificName;
+    }
+  }
 
   return result;
 }
---
+++ test/suppliers/default/util/util.js
@@ -0,0 +1,116 @@
+var mocha = require('mocha');
+var chai = require('chai');
+var sinon = require('sinon');
+var sinonChai = require('sinon-chai');
+
+var util = require('../../../../suppliers/default/util/util');
+
+var expect = chai.expect
+  , sandbox = sinon.sandbox.create()
+  ;
+
+chai.use(sinonChai);
+
+describe('util', () => {
+  describe('#extractClassName', () => {
+    var apiResults;
+
+    context('when hierarchy_entries missing from apiResults', () => {
+      beforeEach(() => {
+        apiResults = {
+          foo: 'bar'
+        }
+      });
+
+      it('throws TypeError', () => {
+        expect(util.extractClassName.bind(apiResults)).to.throw(TypeError);
+      });
+    });
+
+    context('when ancestors is missing', () => {
+      beforeEach(() => {
+        apiResults = {
+          hierarchy_entries: {
+            foo: 'bar'
+          }
+        };
+      });
+
+      it("returns ''", () => {
+        expect(util.extractClassName(apiResults)).to.equal('');
+      });
+    });
+
+    context('when ancestors contains class element with scientificName', () => {
+      beforeEach(() => {
+        apiResults = {
+          hierarchy_entries: {
+            ancestors: [
+              {
+                taxonRank: 'foo'
+              },
+              {
+                taxonRank: 'class',
+                scientificName: 'sci name'
+              },
+              {
+                taxonRank: 'bar'
+              }
+            ]
+          }
+        };
+      });
+
+      it('returns the scientificName', () => {
+        expect(util.extractClassName(apiResults)).to.equal('sci name');
+      });
+    });
+
+    context("when ancestors doesn't contain a class element", () => {
+      beforeEach(() => {
+        apiResults = {
+          hierarchy_entries: {
+            ancestors: [
+              {
+                taxonRank: 'foo'
+              },
+              {
+                taxonRank: 'bar'
+              }
+            ]
+          }
+        };
+      });
+
+      it("returns ''", () => {
+        expect(util.extractClassName(apiResults)).to.equal('');
+      });
+    });
+
+    context(
+      "when ancestors contains a class element that doesn't have a " +
+        "scientificName",
+      () => {
+        beforeEach(() => {
+          apiResults = {
+            hierarchy_entries: {
+              ancestors: [
+                {
+                  taxonRank: 'class'
+                }
+              ]
+            }
+          }
+        });
+
+        it("returns ''", () => {
+          expect(util.extractClassName(apiResults)).to.equal('');
+        });
+      }
+    );
+  });
+
+  afterEach(() => {
+    sandbox.restore();
+  });
+});

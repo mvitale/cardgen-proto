2936ee61af1e01f0194112c3d8d317c128b471bf
Michael Vitale <mmvitale@gmail.com>
2017-07-14 11:28:53 -0400

    add pm2 config directory, misc changes

---
+++ pm2/ecosystem.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  /**
+   * Application configuration section
+   * http://pm2.keymetrics.io/docs/usage/application-declaration/
+   */
+  apps : [
+
+    // Card service
+    {
+      name      : 'card-svc',
+      script    : 'app.js',
+      env: {
+        NODE_ENV: 'development'
+      },
+      env_production : {
+        NODE_ENV: 'production'
+      },
+      error_file      : "err.log",
+      out_file        : "out.log"
+    }
+  ]
+};

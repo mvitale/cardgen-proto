41f15c0ac3794034799f0359c40c23ca6c799524
Michael Vitale <mmvitale@gmail.com>
2017-10-23 16:00:41 -0400

    image-fetcher-test

---
+++ test/image-fetcher-test.js
@@ -0,0 +1,93 @@
+var mocha = require('mocha')
+  , chai = require('chai')
+  , sinon = require('sinon')
+  , sinonChai = require('sinon-chai')
+  ;
+
+var imageFetcher = require('_/image-fetcher')
+  , FetchedImage = require('_/models/fetched-image')
+  , canvas = require('canvas')
+  ;
+
+var expect = chai.expect
+  , sandbox = sinon.sandbox.create()
+  ;
+
+chai.use(sinonChai);
+
+describe('imageFetcher', () => {
+  describe('#fetch', () => {
+    var findOrCreate
+      , cb
+      , url = 'http://www.eol.org/images/1234.jpg'
+      , fetchedImage
+      ;
+
+    beforeEach(() => {
+      findOrCreate = sandbox.stub(FetchedImage, 'findOrCreate');
+      cb = sandbox.spy();
+      fetchedImage = {
+        read: sandbox.stub()
+      };
+    });
+
+    context('success pathway', () => {
+      var rawImage = 'imabuffer'
+        , image
+        ;
+
+      beforeEach(() => {
+        fetchedImage.read.yields(null, rawImage);
+        findOrCreate.yields(null, fetchedImage);
+        image = {
+          src: null
+        };
+        sandbox.stub(canvas, 'Image').returns(image);
+
+        imageFetcher.fetch(url, cb);
+      });
+
+      it('yields the expected result', () => {
+        expect(findOrCreate).to.have.been.calledOnce.calledWith(
+          url,
+          imageFetcher.storageDir
+        );
+        expect(cb).to.have.been.calledOnce.calledWith(null, image);
+        expect(image.src).to.equal(rawImage);
+      });
+    });
+
+    context('when findOrCreate fails', () => {
+      var err = new Error('findOrCreate failed');
+
+      beforeEach(() => {
+        findOrCreate.yields(err);
+
+        imageFetcher.fetch(url, cb);
+      });
+
+      it('yields the error', () => {
+        expect(cb).to.have.been.calledOnce.calledWith(err);
+      });
+    });
+
+    context('when fetchedImage.read fails', () => {
+      var err = new Error('fetchedImage.read failed');
+
+      beforeEach(() => {
+        fetchedImage.read.yields(err);
+        findOrCreate.yields(null, fetchedImage);
+
+        imageFetcher.fetch(url, cb);
+      });
+
+      it('yields the error', () => {
+        expect(cb).to.have.been.calledOnce.calledWith(err);
+      });
+    });
+  });
+
+  afterEach(() => {
+    sandbox.restore();
+  });
+});

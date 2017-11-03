383d30edb4bf5400f06ed250d72df1a082be2397
Michael Vitale <mmvitale@gmail.com>
2017-11-03 15:32:54 -0400

    tests for jobStatus

--- test/unit/deck-pdf-maker-test.js
+++ test/unit/deck-pdf-maker-test.js
@@ -13,23 +13,60 @@ var expect = chai.expect
 chai.use(sinonChai);
 
 describe.only('deck-pdf-maker', () => {
-  describe('startJob', () => {
-    var job = {}
-      , cards = ['card']
-      , logger = {}
-      ;
+  var jobId = 'jobid'
+    , job = {
+        id: jobId 
+      }
+    , cardId1 = 'card1'
+    , cardId2 = 'card2'
+    , card1 = {
+        id: cardId1
+      }
+    , card2 = {
+        id: cardId2
+      }
+    , cards = [card1, card2]
+    , logger = {}
+    , jobError = new Error('job failed')
+    ;
 
-    beforeEach(() => {
-      job.start = sandbox.stub();
-      job.start.returns({
-        then: () => {
-          return {
-            catch: () => {}
-          }
+  function jobSetupBefore() {
+    job.start = sandbox.stub();
+  }
+
+  function setupNonFinishingJob() {
+    jobSetupBefore();
+    job.start.returns({
+      then: () => {
+        return {
+          catch: () => {}
         }
-      });
-      sandbox.stub(pngBatchJob, 'PngBatchJob').returns(job);
+      }
     });
+    jobSetupAfter();
+  }
+
+  function setupResolvingJob() {
+    jobSetupBefore();
+    job.start.resolves({
+      [cardId1]: 'png1',
+      [cardId2]: 'png2'
+    });
+    jobSetupAfter();
+  }
+
+  function setupRejectingJob() {
+    jobSetupBefore();
+    job.start.rejects(jobError);
+    jobSetupAfter();
+  }
+
+  function jobSetupAfter() {
+    sandbox.stub(pngBatchJob, 'PngBatchJob').returns(job);
+  }
+
+  describe('#startJob', () => {
+    beforeEach(setupNonFinishingJob);
 
     it('creates a PngBatchJob and starts it', () => {
       deckPdfMaker.startJob(cards, logger);
@@ -37,6 +74,49 @@ describe.only('deck-pdf-maker', () => {
         .calledWith(cards, logger);
       expect(job.start).to.have.been.calledOnce;
     });
+  });
+
+  describe('#jobStatus', () => {
+    context("when the job has been created but hasn't finished", () => {
+      beforeEach(setupNonFinishingJob);
+
+      it('returns "running"', () => {
+        deckPdfMaker.startJob(cards, logger); 
+        expect(deckPdfMaker.jobStatus(jobId)).to.equal('running'); 
+      });
+    });
+
+    context('when the job resolves', () => {
+      beforeEach(setupResolvingJob);
+
+      it('returns "done"', () => {
+        deckPdfMaker.startJob(cards, logger);
+        process.nextTick(() => {
+          expect(deckPdfMaker.jobStatus(jobId)).to.equal('done');
+        });
+      });
+    });
+
+    context('when the job rejects', () => {
+      beforeEach(setupRejectingJob);
+
+      it('returns "dead"', () => {
+        deckPdfMaker.startJob(cards, logger);
+        process.nextTick(() => {
+          expect(deckPdfMaker.jobStatus(jobId)).to.equal('dead');
+        });
+      });
+    });
+
+    context("when the job doesn't exist", () => {
+      it('returns "dead"', () => {
+        expect(deckPdfMaker.jobStatus('notajobid')).to.equal('dead');
+      });
+    });
+  });
+
+  afterEach(() => {
+    sandbox.restore();
   });
 });
 

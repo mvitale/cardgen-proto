5e1bd0f79611a1f0c25faa7c8b94b3368e9f768c
Michael Vitale <mmvitale@gmail.com>
2018-10-31 12:13:48 -0400

    add worker app to pm2 config

--- pm2/ecosystem.config.js
+++ pm2/ecosystem.config.js
@@ -17,10 +17,26 @@ module.exports = {
         NODE_ENV: 'production',
         watch: false,
         exec_mode: 'cluster',
-        instances: -1
+        instances: 1
       },
       error_file      : "logs/err.log",
       out_file        : "logs/out.log"
+    },
+    {
+      name: 'card-worker',
+      script: 'worker.js'
+      env: {
+        NODE_ENV: 'development',
+        watch: false
+      }, 
+      env_production: {
+        NODE_ENV: 'production',
+        watch: false,
+        exec_mode: 'cluster',
+        instances: 1
+      },
+      error_file: 'logs/worker-err.log',
+      out_file: 'logs/worker-out.log'
     }
   ]
 };
